aac91631f51f44ed9888c4d0d8e6f2676b9e9a7f
Brijendra <bp07072002@gmail.com>
2022-10-06 16:29:56 +0530

    API function for adding new contact

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -21,4 +21,49 @@ export default class {
       });
     }
   }
+
+  static async AddContact(req, res) {
+    try {
+      // validate the input fields, we can use middleware validators, as of now doing validation here.
+
+      if (req.body && !req.body.cname) {
+        return {
+          status: "Error",
+          message: ERR_CUSTOM.cname.message,
+        };
+      }
+      if (req.body && !req.body.email) {
+        return {
+          status: "Error",
+          message: ERR_CUSTOM.email.message,
+        };
+      }
+      if (req.body && !req.body.contact) {
+        return {
+          status: "Error",
+          message: ERR_CUSTOM.contact.message,
+        };
+      }
+      //capture the input data came from the front end.
+      const params = {
+        id: UID(),
+        cname: req.body.cname,
+        email: req.body.email,
+        contact: req.body.contact,
+        cstatus: 0,
+      };
+      //call the Contact service to add the data in database
+      const Contact = await ContactService.AddContact(params);
+      return res.status(HTTPStatus.OK).json({
+        status: "success",
+        message: " Successfully addded Contact",
+        data: Contact,
+      });
+    } catch (error) {
+      return res.status(HTTPStatus[401]).json({
+        status: "Error",
+        message: ERR_CUSTOM[401].message,
+      });
+    }
+  }
 }
--- utils/operationAction.js
+++ utils/operationAction.js
@@ -11,16 +11,10 @@ const saveContactData = (data) => {
   fs.writeFileSync(dataPath, stringifyData);
 };
 
-export const getSingleContactData = (id) => {
-  var existAccounts = getContactData().Contact;
 
-  let foundContact = existAccounts.find((x) => x.id === id);
-  let jsonData = JSON.stringify(foundContact);
-
-  return JSON.parse(jsonData);
-};
 
 export const AddOperationContact = (params) => {
+ 
   var existAccounts = getContactData();
 
   existAccounts.Contact.push({
@@ -34,35 +28,4 @@ export const AddOperationContact = (params) => {
   return saveContactData(existAccounts);
 };
 
-export const UpdateContactDate = (id, params) => {
-  var existContact = getContactData();
 
-  existContact.Contact.find((o) => o.id === id).cname = params.cname;
-  existContact.Contact.find((o) => o.id === id).email = params.email;
-  existContact.Contact.find((o) => o.id === id).contact = params.contact;
-
-  return saveContactData(existContact);
-};
-
-export const DeleteContactOperation = (id) => {
-  var existContact = getContactData();
-  existContact.Contact = existContact.Contact.filter(function (item) {
-    return item.id != id;
-  });
-
-  return saveContactData(existContact);
-};
-
-export const ChangeStatusContactOperation = (id, cstatus) => {
-  var existContact = getContactData();
-  existContact.Contact.find((o) => o.id === id).cstatus = cstatus;
-  return saveContactData(existContact);
-};
-
-export const ViewContactOperation = (id) => {
-  const existContact = getContactData();
-
-  existContact.Contact = getContactData().Contact.find((o) => o.id === id);
-
-  return existContact;
-};
